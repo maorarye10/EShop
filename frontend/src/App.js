c7ce7f0528f8bf5d4844d601b4920f89a776b312
Maor Arie <maorarye10@gmail.com>
2023-07-11 15:16:15 +0300

    First Lesson

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
-import logo from "./logo.svg";
+import { NavLink, Route, Routes } from "react-router-dom";
 import "./App.css";
+import { HomePage } from "./views/HomePage";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="App">
+        <header className="App-header">
+          <NavLink href="/">eshop</NavLink>
+        </header>
+      </div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ frontend/src/data.js
@@ -0,0 +1,42 @@
+const data = {
+  products: [
+    {
+      name: "productA-name",
+      category: "Pharmacy",
+      description: "",
+      price: 100,
+      tocken: "productA-token",
+      brand: "Teva",
+      countInStock: 6,
+      rating: 2.5,
+      reviewsCount: 100,
+      image: "/images/pills.png",
+    },
+    {
+      name: "productB-name",
+      category: "Clothing",
+      description: "",
+      price: 15,
+      tocken: "productB-token",
+      brand: "Castro",
+      countInStock: 12,
+      rating: 3.7,
+      reviewsCount: 54,
+      image: "/images/shirt.png",
+    },
+    {
+      name: "productC-name",
+      category: "Nutrition",
+      description: "",
+      price: 150,
+      tocken: "productC-token",
+      brand: "MyProtein",
+      countInStock: 24,
+      rating: 4.5,
+      reviewsCount: 1000,
+      image: "/images/whey.png",
+    },
+  ],
+};
+
+export default data;
